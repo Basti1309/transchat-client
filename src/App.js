dc4155e926517227f437d290c33c0e764dd2d4dd
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-07-08 23:48:44 +0300

    feat: Redux, Socket io connection

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import './index.css';
 import Header from './components/Header';
 import ChatScreen from './components/ChatScreen';
 import SpeechInput from './components/SpeechInput';
-import SpeechButton from './components/SpeechButton';
+
+
+
+// import SpeechButton from './components/SpeechButton';
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
         <Header />
         <ChatScreen />
         <SpeechInput />
-        <SpeechButton />
+
       </div>
     );
   }
--- src/components/SpeechButton.js
+++ src/components/SpeechButton.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import '../index.css';
+import { textToStore } from '../actions';
+import socketIOClient from 'socket.io-client';
 
 class SpeechButton extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
+
   }
 
+  // handleOnClick = () => {
+  //   this.socket.emit('SEND_TEXT_MESSAGE', {  // send text to the server
+  //       message: this.props.text,
+  //     });
+  //   this.props.textToStore(this.props.text);
+  //   this.props.clear();
+  // };
+
   render() {
+
     return (
       <div className="speech-button">
         <Button icon style={{ backgroundColor: 'white' }}>
@@ -16,6 +29,7 @@ class SpeechButton extends Component {
         <Button
           animated
           size="huge"
+          // onClick={() => this.handleOnClick()}
           >
           <Button.Content visible>Send</Button.Content>
           <Button.Content hidden>
@@ -27,4 +41,12 @@ class SpeechButton extends Component {
   }
 }
 
-export default SpeechButton;
+// const mapDispatchToProps = dispatch => ({
+//   textToStore: (text) => dispatch(textToStore(text)),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   textToScreen: () => dispatch(textToScreen()),
+// });
+
+export default connect(null, mapDispatchToProps)(SpeechButton);
