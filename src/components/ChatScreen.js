19d8abdf210e9c23e5c1b1b421d51a34c5306d5e
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-07-12 13:28:32 +0300

    feat:Message component add & bubbles direction set

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../index.css';
-const uuidv4= require('uuid/v4');
+import Message from './Message';
+const uuidv4 = require('uuid/v4');
 
 class ChatScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-  }
-
   render() {
-    console.log(this.props.messages);
+    const messages = this.props.messages.map(message => {
+          return (
+      
+            <Message
+              key={uuidv4()}
+              messageInfo={message}
+            />
+
+          );
+        });
     return (
-      <div>
         <div className="chat-wrapper">
-          {this.props.messages.map(message => {
-            return (
-              <div className="messages" key={uuidv4()}>{message}</div>
-            );
-          })}
+         {messages}
         </div>
-      </div>
     );
   }
 }
--- src/reducers/textReducer.js
+++ src/reducers/textReducer.js
@@ -1,7 +1,13 @@
 export default(state = [], action) => {
   switch (action.type) {
     case 'TEXT_TO_STORE':
-      return [...state, action.message];
+      return state.concat([
+        {
+          message: action.message.text,
+          serverId: action.message.socketId,
+          clientId: action.message.clientId,
+        },
+      ]);
     default:
       return state;
   }
